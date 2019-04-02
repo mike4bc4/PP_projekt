e6c3de169f33423e035b5d95aabf8a973f66907c
Next0r <mchkafka@gmail.com>
2019-04-02 18:46:16 +0200

    aaa

---
+++ BedAndBreakfast/BedAndBreakfast/Controllers/HelpController.cs
@@ -0,0 +1,57 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+using BedAndBreakfast.Data;
+using Microsoft.AspNetCore.Mvc;
+
+namespace BedAndBreakfast.Controllers
+{
+	/// <summary>
+	/// This class shoudl be used to call methods realted to help pages like searching and displaying help page.
+	/// </summary>
+    public class HelpController : Controller
+    {
+		private AppDbContext context;
+
+		public HelpController(AppDbContext context) {
+			this.context = context;
+		}
+
+		/// <summary>
+		/// Displays search page.
+		/// </summary>
+		/// <returns></returns>
+		public IActionResult Search() {
+			// TODO: Pass to ViewData list of FAQ
+			// View displays saerch box and FAQ
+
+			return View();
+		}
+
+		/// <summary>
+		/// Performs search for help pages in database.
+		/// </summary>
+		/// <param name="searchString"></param>
+		/// <returns></returns>
+		public IActionResult Search(string searchString) {
+
+			// TODO: Return list of search results.
+			// TODO: Redirect to view which allows to choose result (hyperling which redirects to DisplayPage action with page reference)
+			return View();
+		}
+
+		/// <summary>
+		/// Redirects to view with specified help page based on help page reference.
+		/// </summary>
+		/// <param name="pageID"></param>
+		/// <returns></returns>
+		public IActionResult DisplayPage(HelpPage page) {
+			// TODO: Display page in suitable form.
+			return View();
+		}
+
+		
+
+    }
+}
---
+++ BedAndBreakfast/BedAndBreakfast/Data/HelpPage.cs
@@ -0,0 +1,48 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+
+namespace BedAndBreakfast.Data
+{
+	/// <summary>
+	/// This class represents help file section which contains title and content as text.
+	/// </summary>
+	public class HelpFileSection {
+
+		public string title { get; set; }
+		public string content { get; set; }
+
+		public HelpFileSection(string title, string content) {
+			this.title = title;
+			this.content = content;
+		}
+
+	}
+
+	/// <summary>
+	/// This class represents help page entity.
+	/// </summary>
+	public class HelpPage
+	{
+		/// <summary>
+		/// Help page ID
+		/// </summary>
+		public int ID { get; set; }
+
+		/// <summary>
+		/// Tags used to find help page
+		/// </summary>
+		public List<string> Tags { get; set; }
+
+		/// <summary>
+		/// Title of help page
+		/// </summary>
+		public string Title { get; set; }
+
+		/// <summary>
+		/// Sections contained in help file.
+		/// </summary>
+		public List<HelpFileSection> FileSections { get; set; }
+	}
+}
