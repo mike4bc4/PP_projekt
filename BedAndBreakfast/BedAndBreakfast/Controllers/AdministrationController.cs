c468ae1f162bfcb63cd80bab0dfc4f4af73b3a56
Michał Kafka <mchkafka@gmail.com>
2019-04-12 17:46:03 +0200

    Empty administration controller added.

---
+++ BedAndBreakfast/BedAndBreakfast/Controllers/AdministrationController.cs
@@ -0,0 +1,24 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+using BedAndBreakfast.Data;
+using Microsoft.AspNetCore.Authorization;
+using Microsoft.AspNetCore.Mvc;
+
+namespace BedAndBreakfast.Controllers
+{
+    /// <summary>
+    /// This controller is used to provide all methods necessary to 
+    /// perform web service administration duties.
+    /// Only administrator has access to these.
+    /// </summary>
+    [Authorize(Roles = Role.Admin)]
+    public class AdministrationController : Controller
+    {
+        
+
+
+
+    }
+}
